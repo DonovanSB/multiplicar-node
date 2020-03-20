42792ec722771ed8977682999f06f0f395c0ae61
Donovan <donovan.dsb@gmail.com>
2020-03-20 10:57:19 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,32 @@
+// requireds
+// const fs = require('express');
+// const fs = require('./fs');
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+
+// let argv2 = process.argv;
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+            .then(archivo => console.log(archivo))
+            .catch(e => console.log(e))
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado:`, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
+
+console.log(argv);
+
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
